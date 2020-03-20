4f7aeadce08d654164e6b5e0e10af503b1ca832f
Konrad <57108310+KonradGmur@users.noreply.github.com>
2020-03-20 15:28:24 +0100

    Add my new page

---
+++ js/main.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const nav = document.querySelector('.navbar')
+
+    function addShadow() {
+        if (window.scrollY >= 200) {
+            nav.classList.add('shadow-bg')
+        } else {
+            nav.classList.remove('shadow-bg')
+        }
+    }
+    window.addEventListener('scroll', addShadow)
+
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("show");
+        if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+            $(".navbar-toggler").click();
+        }
+    });
+
+});
